dc4b1058f59d7e2fee51a7c71801ca5c765e8975
Liza Star <lizadstar18@gmail.com>
2020-02-22 10:14:44 -0600

    creating new dino object

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-const dinos = [
-    {
-      id: 'dino1',
-      name: 'Rex',
-      type: 'T Rex',
-      age: 100,
-      owner: 'Zoe',
-      adventures: [],
-      health: 100,
-      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
-    }
-  ]
+const dinos = [];
 
-  const newDino = () => {
+const newDino = (e) => {
+    e.preventDefault();
+    const brandNewDino = {
+    id: `dino${dinos.length + 1}`,
+    name: document.getElementById('dino-name').value,
+    type: document.getElementById('dino-type').value,
+    age: document.getElementById('dino-age').value,
+    owner: document.getElementById('dino-owner').value,
+    adventures: [],
+    health: 100,
+    imageUrl: document.getElementById('dino-image').value
+    };
+dinos.push(brandNewDino);
+console.log('New Dino', dinos);
+};
 
-  };
- 
-  const formCollapse = () => {
+const init = () => {
+    document.getElementById('submit-new-dino').addEventListener('click', newDino);
+};
 
-  }; 
+init();
