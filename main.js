3d31da214d27ca25d2c94f25a6df2a20342b1df1
Liza Star <lizadstar18@gmail.com>
2020-02-21 22:29:30 -0600

    wip

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('hey');
+const dinos = [
+    {
+      id: 'dino1',
+      name: 'Rex',
+      type: 'T Rex',
+      age: 100,
+      owner: 'Zoe',
+      adventures: [],
+      health: 100,
+      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+    }
+  ]
